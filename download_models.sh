e68adfc07386da7148d0c6eb506ab9acc2b9984e
Lahav Lipson <llipson@princeton.edu>
2026-08-03 15:21:39 -0400

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir models -p
 cd models
-wget https://www.dropbox.com/s/ftveifyqcomiwaq/models.zip
+wget "https://www.dropbox.com/scl/fi/5khx1bhz84dapi8vtwapg/models.zip?rlkey=ggddrn1du1iiq6mgc2dsdpmwi&dl=1" -O models.zip
 unzip models.zip
 rm models.zip -f
